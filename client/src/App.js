e53e20381c388e5aa5c7f13152c5be80552e82de
unknown <tsast@gmail.com>
2024-04-29 14:51:23 +0100

    adding pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebarr from './components/sidebar/sidebar';
 import './App.css'
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
     <Sidebarr />
+    <Login/>
     </>
   )
 }
