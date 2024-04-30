ecc86c6fc74db462aa1902e65dccf346264860eb
Ouael Hashassi <90405499+Oua2eL@users.noreply.github.com>
2024-04-30 11:16:24 +0100

    added notification bar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/navbar.css';
 
 const Navbar = ({ username }) => {
-    return (
-      <div className="navbar">
-        <div className="notification-icon">
-          <i className="fas fa-bell"></i>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="notification-icon" onClick={handleMenuToggle}>
+        <i className="fas fa-bell"></i>
+      </div>
+      <div className={`side-menu ${isMenuOpen ? 'open' : ''}`}>
+        {/* Your notification content goes here */}
+        <div className="notification-content">
+          {/* Notification items */}
+          <div className="notification-item">
+            <div className="user-avatar">
+              <img src="./logo192.png" alt="User" className="avatar-image" />
+            </div>
+            <div className="notification-details">
+              <div className="notification-message">
+                <span className="user-name">John Doe</span> liked your post
+              </div>
+              <div className="notification-time">2 hours ago</div>
+            </div>
+          </div>
+          <div className="notification-item">
+            <div className="user-avatar">
+              <img src="./logo192.png" alt="User" className="avatar-image" />
+            </div>
+            <div className="notification-details">
+              <div className="notification-message">
+                <span className="user-name">Jane Smith</span> commented on your photo
+              </div>
+              <div className="notification-time">1 day ago</div>
+            </div>
+          </div>
         </div>
-        <div className="user-profile">
-            <img src="./logo192.png" alt="User" className="user-image" title={`Hello, ${username}`} />
+        <div className="close-button" onClick={handleMenuToggle}>
+          <i className="fas fa-times"></i>
         </div>
       </div>
-    );
-  }
+      <div className="user-profile">
+        <img src="./logo192.png" alt="User" className="user-image" title={`Hello, ${username}`} />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
