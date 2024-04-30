2c8f839b144bab0a9f9388afb028422351056bf5
mahdisouki <mahdisouki88@gmail.com>
2024-04-30 21:33:48 +0100

    adding login page and auth system

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/sidebar';
-import './App.css'
+import './App.css';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
-import Categories from './pages/catCreate'
-import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import Categories from './pages/catCreate';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
+
+
   return (
     <Router>
-    <div className='app-container'>
-      <div className='sidebar-container'>
-        <Sidebar />
-      </div>
-      <div className='content-container'>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/CreationCat' element={<Categories />} />
-        </Routes>
-      </div>
-    </div>
-  </Router>
-)
-}
+      <div className='app-container'>
+        {!token && (
+          <div className='sidebar-container'>
+            <Sidebar />
+          </div>
+        )}
+        {!token && (
+          <div className='content-container'>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path='/CreationCat' element={<Categories />} />
+            </Routes>
+          </div>
+        )}
+        </div>
+        {token && <Login setToken={setToken} />}
+      
+    </Router>
+  );
+};
 
-export default App
-
+export default App;
