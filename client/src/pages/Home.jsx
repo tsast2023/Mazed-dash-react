859b3f99e4414a4ed31a60cfc9e3b0e28a8d2d14
Ouael Hashassi <90405499+Oua2eL@users.noreply.github.com>
2024-05-07 12:04:36 +0100

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,173 +1,160 @@
-import React, { useEffect, useRef } from "react";
-import ApexCharts from "apexcharts";
+import React, { useEffect, useRef, useState } from 'react';
+import ApexCharts from 'apexcharts';
 
 const Dashboard = () => {
   const barChartRef = useRef(null);
   const lineChartRef = useRef(null);
   const candleChartRef = useRef(null);
+  const [isChartsInitialized, setIsChartsInitialized] = useState(false);
 
   useEffect(() => {
-    const options1 = {
-      series: [
-        {
+    if (!isChartsInitialized && barChartRef.current && lineChartRef.current && candleChartRef.current) {
+      const optionsBar = {
+        series: [{
           name: "Installation",
-          data: [112000],
+          data: [112000]
+        }],
+        chart: {
+          type: 'bar',
+          height: 350
         },
-      ],
-      chart: {
-        type: "bar",
-        height: 350,
-        parentHeightOffset: 0,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
+        plotOptions: {
+          bar: {
+            horizontal: false,
+          },
         },
-      },
-      xaxis: {
-        categories: ["Nombre d'installation"],
-      },
-    };
+        xaxis: {
+          categories: ['Nombre d\'installation'],
+        }
+      };
 
-    const options2 = {
-      series: [
-        {
+      const optionsLine = {
+        series: [{
           name: "Désinstallations",
-          data: [183000],
+          data: [183000]
+        }],
+        chart: {
+          type: 'line',
+          height: 350
         },
-      ],
-      chart: {
-        type: "line",
-        height: 350,
-        parentHeightOffset: 0,
-      },
-      xaxis: {
-        categories: ["Nombre de désinstallations"],
-      },
-    };
+        xaxis: {
+          categories: ['Nombre de désinstallations'],
+        }
+      };
 
-    const options3 = {
-      series: [
-        {
+      const optionsCandle = {
+        series: [{
           name: "Inscriptions",
-          data: [80000],
+          data: [
+            {
+              x: new Date(1538778600000),
+              y: [6629.81, 6650.5, 6623.04, 6633.33]
+            }
+          ]
+        }],
+        chart: {
+          type: 'candlestick',
+          height: 350
         },
-      ],
-      chart: {
-        type: "candlestick",
-        height: 350,
-        parentHeightOffset: 0,
-      },
-      xaxis: {
-        categories: ["Nombre d'inscriptions"],
-      },
-    };
+        xaxis: {
+          categories: ['Nombre d\'inscriptions'],
+        }
+      };
 
-    if (barChartRef.current && lineChartRef.current && candleChartRef.current) {
-      const barChart = new ApexCharts(barChartRef.current, options1);
-      const lineChart = new ApexCharts(lineChartRef.current, options2);
-      const candleChart = new ApexCharts(candleChartRef.current, options3);
+      // Creating chart instances
+      new ApexCharts(barChartRef.current, optionsBar).render();
+      new ApexCharts(lineChartRef.current, optionsLine).render();
+      new ApexCharts(candleChartRef.current, optionsCandle).render();
 
-      barChart.render();
-      lineChart.render();
-      candleChart.render();
+      setIsChartsInitialized(true);  // Set the flag to true after initializing charts
     }
-  }, []);
+  }, [barChartRef.current, lineChartRef.current, candleChartRef.current]);
 
   return (
-    <div className="content-container">
       <div id="main">
         <div className="page-content">
           <section className="row">
-            <div className="col-12">
-              <div className="row">
-                <div className="col-4">
-                  <div className="card">
-                    <div className="card-body px-4 py-4-5">
-                      <div className="row">
-                        <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                          <div className="stats-icon purple mb-2">
-                            <i className="fa-solid fa-download"></i>
-                          </div>
-                        </div>
-                        <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                          <h6 className="text-muted font-semibold">
-                            Nombre d'installation
-                          </h6>
-                          <h6 className="font-extrabold mb-0">112.000</h6>
-                        </div>
+          <div className='card-stats'>
+            <div className="col-4">
+              <div className="card">
+                <div className="card-body px-4 py-4-5">
+                  <div className="row">
+                    <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                      <div className="stats-icon purple mb-2">
+                        <i className="fa-solid fa-download "></i>
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="card">
-                    <div className="card-body px-4 py-4-5">
-                      <div className="row">
-                        <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                          <div className="stats-icon blue mb-2">
-                            <i className="fa-solid fa-trash-can"></i>
-                          </div>
-                        </div>
-                        <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                          <h6 className="text-muted font-semibold">
-                            Nombre de désinstallations
-                          </h6>
-                          <h6 className="font-extrabold mb-0">183.000</h6>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div className="card">
-                    <div className="card-body px-4 py-4-5">
-                      <div className="row">
-                        <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                          <div className="stats-icon green mb-2">
-                            <i className="fa-solid fa-user-plus"></i>
-                          </div>
-                        </div>
-                        <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                          <h6 className="text-muted font-semibold">
-                            Nombre d'inscriptions
-                          </h6>
-                          <h6 className="font-extrabold mb-0">80.000</h6>
-                        </div>
-                      </div>
+                    <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                      <h6 className="text-muted stats-text">Nombre d'installation</h6>
+                      <h6 className="stats-number mb-0">112.000</h6>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="card">
-                    <div className="card-header">
-                      <h4>Traffic sur les produits</h4>
+            </div>
+            <div className="col-4">
+              <div className="card">
+                <div className="card-body px-4 py-4-5">
+                  <div className="row">
+                    <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                      <div className="stats-icon blue mb-2">
+                        <i className="fa-solid fa-trash-can"></i>
+                      </div>
                     </div>
-                    <div className="card-body">
-                      <div ref={barChartRef}></div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div className="card">
-                    <div className="card-header">
-                      <h4>Traffic sur les enchères</h4>
-                    </div>
-                    <div className="card-body">
-                      <div ref={lineChartRef}></div>
+                    <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                      <h6 className="text-muted stats-text">Nombre de désinstallations</h6>
+                      <h6 className="stats-number mb-0">183.000</h6>
                     </div>
                   </div>
                 </div>
-                <div className="col-md-4">
-                  <div className="card">
-                    <div className="card-header">
-                      <h4>Traffic sur les catégories</h4>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="card">
+                <div className="card-body px-4 py-4-5">
+                  <div className="row">
+                    <div className="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                      <div className="stats-icon green mb-2">
+                        <i className="fa-solid fa-user-plus"></i>
+                      </div>
                     </div>
-                    <div className="card-body">
-                      <div ref={candleChartRef}></div>
+                    <div className="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                      <h6 className="text-muted stats-text">Nombre d'inscriptions</h6>
+                      <h6 className="stats-number mb-0">80.000</h6>
                     </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card">
+                  <div className="card-header">
+                    <h4>Traffic sur les produits</h4>
+                  </div>
+                  <div className="card-body">
+                    <div ref={barChartRef}></div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card">
+                  <div className="card-header">
+                    <h4>Traffic sur les enchères</h4>
+                  </div>
+                  <div className="card-body">
+                    <div ref={lineChartRef}></div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card">
+                  <div className="card-header">
+                    <h4>Traffic sur les catégories</h4>
+                  </div>
+                  <div className="card-body chart">
+                    <div ref={candleChartRef}></div>
                   </div>
                 </div>
               </div>
@@ -175,7 +162,6 @@ const Dashboard = () => {
           </section>
         </div>
       </div>
-    </div>
   );
 };
 
