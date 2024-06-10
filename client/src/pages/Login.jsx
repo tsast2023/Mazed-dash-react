b79e6da98370dd6fab952e2bd83a304f1f288a23
Ouael Hashassi <90405499+Oua2eL@users.noreply.github.com>
2024-06-10 22:06:51 +0100

    login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,43 +18,35 @@ export default function Login() {
 
   return (
     <div className="container-fluid">
-      <div className="row justify-content-center"> {/* Center the left part */}
-        {/* Left Part */}
-        <div className="col-lg-6 col-md-12">
-          <div className="login-form">
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder="Enter email"
-                  value={user.email}
-                  onChange={(e) => setUser({ ...user, email: e.target.value })}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="Enter password"
-                  value={user.password}
-                  onChange={(e) => setUser({ ...user, password: e.target.value })}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary btn-block">Login</button>
-            </form>
-          </div>
-        </div>
-        {/* Right Part */}
-        <div className="col-lg-6 d-none d-lg-block">
-          <div className="login-image">
-            <img src="./Mazed.jpg" alt="Login" />
-          </div>
+      <div className="background-image"></div>
+      <div className="login-form-container">
+        <div className="login-form">
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block">Login</button>
+          </form>
         </div>
       </div>
     </div>
