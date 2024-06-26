be02360a0af43d4846c5fef4be17f92a1d391030
mahdisouki <soukimahdi@gmail.com>
2024-06-26 19:29:14 +0100

    <wxcvbn,;

--- client/src/pages/ListeAdministrateur.jsx
+++ client/src/pages/ListeAdministrateur.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from 'react'
 import Swal from "sweetalert2";
 
 function ListeAdministrateur() {
